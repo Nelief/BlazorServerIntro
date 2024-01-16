dec1ce2f82b013c37d8c80b6f5d7e9d6a800a922
mauro.malafronte <mauriade.mal@gmail.com>
2024-01-16 15:25:29 +0100

    Aggiungere i file di progetto.

---
+++ BlazorServer/BaseComponents/DateBaseComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Timers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BlazorServer.BaseComponents
+{
+    public class DateBaseComponent:ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+        System.Timers.Timer dateTimer;
+
+        public DateBaseComponent()
+        {
+            dateTimer = new System.Timers.Timer(1000)
+            {
+                Enabled = true,
+                AutoReset = true
+            };
+            dateTimer.Elapsed += (sender, e) => Tick();
+        }
+
+        private void Tick()
+        {
+            dateTime = DateTime.Now;
+            InvokeAsync(() =>
+            {
+                StateHasChanged();
+            });
+        }
+
+        public void Dispose()
+        {
+            dateTimer?.Dispose();
+        }
+    }
+}
